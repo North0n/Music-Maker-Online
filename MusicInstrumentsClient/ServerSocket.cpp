657c698a5d8153ab3b07811c64cd69dba337bd73
North0n <mautin.ivan.3@gmail.com>
2022-05-29 21:17:17 +0300

    ServerSocket is created

---
+++ MusicInstrumentsClient/ServerSocket.cpp
@@ -0,0 +1,38 @@
+#include "ServerSocket.h"
+
+ServerSocket::ServerSocket(quint16 port, const QHostAddress& serverAddress, quint16 serverPort, QObject* parent)
+	: QObject(parent), mPort(port), mServerAddress(serverAddress), mServerPort(serverPort)
+{
+	mSocket = std::make_unique<QUdpSocket>(this);
+	mSocket->bind(QHostAddress::Any, port);
+	connect(mSocket.get(), &QUdpSocket::readyRead, this, &ServerSocket::getData);
+}
+
+ServerSocket::~ServerSocket()
+{
+	quit();
+}
+
+void ServerSocket::sendShortMsg(quint32 msg)
+{
+	QByteArray data;
+	QDataStream command(&data, QIODevice::WriteOnly);
+	command << static_cast<quint8>(Commands::ShortMsg)
+			<< msg;
+	mSocket->writeDatagram(data, mServerAddress, mServerPort);
+}
+
+void ServerSocket::quit()
+{
+	QByteArray data;
+	QDataStream command(&data, QIODevice::WriteOnly);
+	command << static_cast<quint8>(Commands::Quit);
+	mSocket->writeDatagram(data, mServerAddress, mServerPort);
+}
+
+void ServerSocket::getData()
+{
+	QByteArray bytes(mSocket->pendingDatagramSize(), '\0');
+	mSocket->readDatagram(bytes.data(), mSocket->pendingDatagramSize());
+	emit dataReceived(bytes);
+}
---
+++ MusicInstrumentsClient/ServerSocket.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <QObject>
+#include <QHostAddress>
+#include <QUdpSocket>
+#include <memory>
+#include <QByteArray>
+
+class ServerSocket : public QObject
+{
+	Q_OBJECT
+
+public:
+
+	enum class Commands
+	{
+		ShortMsg,
+		Quit,
+	};
+
+	ServerSocket(quint16 port, const QHostAddress& serverAddress, quint16 serverPort, QObject* parent = nullptr);
+
+	~ServerSocket();
+
+	void setServerPort(quint16 port) { mServerPort = port; }
+
+	void sendShortMsg(quint32 msg);
+
+	void quit();
+
+signals:
+	void dataReceived(const QByteArray &data);
+
+private:
+
+	void getData();
+
+	QHostAddress mServerAddress;
+	quint16 mServerPort;
+
+	// Port which receiver listens to
+	quint16 mPort;
+	std::unique_ptr<QUdpSocket> mSocket;
+};
+
